38b55a7eeb5234a903d8469855902b6d297761aa
Tobi Beernaert <tobi.beernaert@devoteam.com>
2020-07-31 12:12:51 +0200

    :recycle: Make our models extend from Model

--- src/models/meal.js
+++ src/models/meal.js
@@ -1,35 +1,34 @@
 "use strict";
 
-import { DataTypes } from "sequelize";
-import sequelize from "../config/storage";
 import { v4 as uuidv4 } from "uuid";
 import { isValidType } from "./MealType";
 import User from "./user";
 
-const Meal = sequelize.define("Meal", {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-    autoIncrement: false
-  },
-  type: {
-    type: DataTypes.STRING,
-    validate: (value) => {
-      if (!isValidType(value)) {
-        throw new Error(`${value} is not a valid type`);
-      }
+export default (sequelize, DataTypes) => {
+  const Meal = sequelize.define("Meal", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      autoIncrement: false
     },
-  },
-  what: DataTypes.STRING,
-  when: DataTypes.DATE,
-  where: DataTypes.STRING,
-}, {});
+    type: {
+      type: DataTypes.STRING,
+      validate: (value) => {
+        if (!isValidType(value)) {
+          throw new Error(`${value} is not a valid type`);
+        }
+      },
+    },
+    what: DataTypes.STRING,
+    when: DataTypes.DATE,
+    where: DataTypes.STRING,
+  }, {});
 
-Meal.beforeCreate(meal => meal.id = uuidv4());
+  Meal.beforeCreate(meal => meal.id = uuidv4());
 
-Meal.belongsTo(User, {foreignKey: "user", targetKey: "id"});
+  Meal.belongsTo(User, {foreignKey: "user", targetKey: "id"});
 
-
-export default Meal;
+  return Meal;
+};
--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,46 @@
 "use strict";
 
-import { DataTypes } from "sequelize";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
-import sequelize from "../config/storage";
 import Meal from "./meal";
 
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-    autoIncrement: false
-  },
-  name: {
-    type: DataTypes.STRING
-  },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: true
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      autoIncrement: false
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    pass: {
+      type: DataTypes.STRING
     }
-  },
-  pass: {
-    type: DataTypes.STRING
-  }
-}, {});
+  }, {});
 
-User.beforeCreate(user => user.id = uuidv4());
+  User.beforeCreate(user => user.id = uuidv4());
 
-User.beforeCreate(user => {
-  return bcrypt.hash(user.pass, 10)
-    .then(hash => {
-      user.pass = hash;
-    })
-    .catch(err => {
-      console.log(err);
-      throw new Error();
-    });
-});
+  User.beforeCreate(user => {
+    return bcrypt.hash(user.pass, 10)
+      .then(hash => {
+        user.pass = hash;
+      })
+      .catch(err => {
+        console.log(err);
+        throw new Error();
+      });
+  });
 
-User.hasMany(Meal, { as: "meals", foreignKey: "fk_mealid", sourceKey: "id" });
+  User.hasMany(Meal, { as: "meals", foreignKey: "fk_mealid", sourceKey: "id" });
 
-export default User;
+  return User;
+};
