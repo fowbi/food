52a6f10c3a26b072fdca70b518171cecdc7fa884
Tobi Beernaert <tobi.beernaert@devoteam.com>
2020-07-31 12:13:55 +0200

    :sparkles: Add routing config

---
+++ src/config/routes.js
@@ -0,0 +1,16 @@
+const routeConfig = function (app) {
+  app.use((error, request, response, next) => {
+    if (error.message && (~error.message.indexOf("not found") || ~error.message.indexOf("Cast to ObjectId failed"))) {
+      return next();
+    }
+
+    console.error(error.stack);
+    response.status(500).json({ error: error.stack });
+  });
+
+  app.use((request, response) => {
+    response.status(404).json({ message: "Not found" });
+  });
+};
+
+export default routeConfig;
--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@
 import express from "express";
 import config from "./config";
 import expressConfig from "./config/express";
+import routeConfig from "./config/routes";
 
 const app = express();
 
 expressConfig(app);
-
+routeConfig(app);
 
 app.listen(config.port, () => console.log("Server started listening on " + config.port));
