984415bcdedde11aac9caeed9890617c6fb76d3c
Tobi Beernaert <tobi.beernaert@devoteam.com>
2020-08-24 12:45:06 +0200

    :sparkles: Sort meal results by when parameter

--- src/app/user/Http/GetMeals.js
+++ src/app/user/Http/GetMeals.js
@@ -2,14 +2,25 @@ import moment from "moment";
 import { models, operation } from "../../../config/storage";
 
 const action = async (request, response, next) => {
-  const when = moment(request.params.when).utc();
+  const when = moment(request.params.when);
   const startDate = when.clone().startOf("day");
   const endDate = when.clone().endOf("day");
 
   // temp: all meals created will be attached to the seeded demo user.
   const user = await models.user.findOne({ where: { name: "demo" } });
 
-  await models.meal.findAll({ where: { user: user.id, when: { [operation.gte]: startDate.format(), [operation.lte]: endDate.format() } } })
+  await models.meal.findAll({
+    where: {
+      user: user.id,
+      when: {
+        [operation.gte]: startDate.format(),
+        [operation.lte]: endDate.format(),
+      },
+    },
+    order: [
+      ["when", "DESC"],
+    ],
+  })
     .then(function(meals) {
       response.status(200).send(meals);
     })
